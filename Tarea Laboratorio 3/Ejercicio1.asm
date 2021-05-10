6768aa16c4c3d3999d001e6dcb8e65703aea3d50
ismael712 <vladimirgarcia712@gmail.com>
2021-05-10 00:54:28 -0600

    Modificando ejercicio 1 de la tarea labo 3

--- Tarea Laboratorio 3/Ejercicio1.asm
+++ Tarea Laboratorio 3/Ejercicio1.asm
@@ -2,41 +2,35 @@ org 100h
 
 section .text
 
-    XOR AL,AL;
-    XOR BL,BL;
-    MOV AL, 0d;
-    MOV BL, 0d;
-    call sumar
-    call dividir
-    call exit
+    XOR AX, AX
+    XOR BX,BX;
+    XOR SI, SI;
+    MOV BL, 8D;           cantidad de digitos del carnet, que sera utilizado para la division
+    MOV word CX, 8D;      Contador que es de por si de parte del loop a la cual le asignamos 8 ya que es la cantidad de recorridos que hara, disminuyendo en 1
+    MOV SI, 00;           contador que lo inicializamos en 0, ya que por cada pasada se estaria moviendo una casilla, ejemplo-> iteracion 1 : [200+0], iteracion 2: [200+1]....
+    MOV byte [0200H], 00; moviendo los digitos a una casilla de memoria para asi facilitarnos el ocupar un bucle 
+    MOV byte [0201H], 00;
+    MOV byte [0202H], 00;
+    MOV byte [0203H], 03;
+    MOV byte [0204H], 06;
+    MOV byte [0205H], 08;
+    MOV byte [0206H], 01;
+    MOV byte [0207H], 08;                        
 
-sumar:
-    ADD AL, 0d; ;AL =0+0
-    ADD BL, AL; ;AL = 0+0  BL = 0 -> BL = 0+0+0 -> BL = 0;
-    MOV AL, 3d; ;AL = 3
-    ADD AL, BL; ;AL = 3   BL = 0; -> AL = 3 + 0
-    MOV BL, 6d; ;BL = 6
-    ADD BL, AL; ;BL = 6   AL = 3 -> BL = 3 + 6 = 9
-    MOV AL, 8d; ;AL = 8
-    ADD AL, BL; ;AL = 8   BL = 9 -> AL = 9+8 = 17
-    MOV BL, 1d; ;BL = 1
-    ADD BL, AL; ;BL = 1   AL = 17 -> BL = 17+1 = 18
-    MOV AL, 8d; ;AL = 8
-    ADD AL, BL; ;AL = 8   BL = 18 -> AL = 18+8 = 26
-    ;MOV [20Ah],AL; ; 20Ah = 26
-    CMP AL, 26d; ; compara si es igual a 26d, el programa returnara y llamara a ELALIT
-    JNE sumar; si es igual se seguira ejecutando sumar
-    ret; retornamo
+    jmp iterar           ;vamos hacer un salto hacia el bucle
+
+
+iterar:
+    ADD AL, [0200H + SI]; se va ir sumando cada casilla de memoria y se ira almacenando en AL
+    INC SI; incrementamos el contador de nuestro indice
+    LOOP iterar; se repetira hasta que nuestro contador CX disminuya a 0
 
 dividir:
-    MOV [200h], AL;
-    MOV AX, [200h];
-    MOV BL, 8d;
-    IDIV BL; ya que estamos trabajando con valores de 8bits se utiliza bits con signo por el primer bit significativo
-    MOV byte [20Ah], AL;
-    CMP AL, 3d;
-    JNE dividir;
-    ret;
+    DIV BL;
+    MOV [020AH], AL;
+    jmp exit;
 
 exit:
-    int 20h ; terminamos el programa
+    int 20h;
+
+
